7e71470a6d43fec38e586d5619d7d12f2d5f367b
Travis Mack <travismack47@protonmail.com>
2023-04-20 14:17:14 -0500

    created function an object, added albums to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,25 @@
 console.log('***** Music Collection *****')
+
+
+let collection = [];
+
+function addToCollection( title, artist, yearPublished ) {
+    let album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(album);
+    return album;
+}
+
+addToCollection('Teflon Don', 'Rick Ross', 2010);
+console.log(collection);
+
+addToCollection('Finally Rich', 'Chief Keef', 2012);
+addToCollection('Painting Pictures', 'Kodak Black', 2017);
+addToCollection('Magna Carta Holy Grail', 'Jay-Z', 2013);
+addToCollection('The Persona Tape', 'Maxo Kream', 2016);
+addToCollection('Time Served', 'Moneybagg Yo', 2020);
+
+console.log(collection);
